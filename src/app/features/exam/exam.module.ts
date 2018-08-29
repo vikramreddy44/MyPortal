0eeb7f2dd2905d5c2f720f8fd78f922e2713b3b3
Vikram.reddy <vikramreddy.44@gmail.com>
2018-08-29 22:05:21 +0530

    Commit 2

--- src/app/features/exam/exam.module.ts
+++ src/app/features/exam/exam.module.ts
@@ -12,6 +12,7 @@ import { HomeComponent } from './home/home.component';
     ExamRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+	
   ],
   declarations: [ExamComponent, HomeComponent]
 
